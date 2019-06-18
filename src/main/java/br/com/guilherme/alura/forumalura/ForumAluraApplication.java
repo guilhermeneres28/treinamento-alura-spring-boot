25bf1ca43e0b9b7cec93ee19b78721ac99d6a63d
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-18 15:38:01 -0300

    Inicio do projeto do treinamento

---
+++ src/main/java/br/com/guilherme/alura/forumalura/ForumAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.alura.forumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumAluraApplication.class, args);
+	}
+
+}
