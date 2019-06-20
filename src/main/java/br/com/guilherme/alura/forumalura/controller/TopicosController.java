944f0c37d1e84a530fb0eef09337ac307e424c1e
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-20 20:27:48 -0300

    Adequando a controller para passar a ser uma rest controller

--- src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
+++ src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
@@ -5,17 +5,17 @@ import br.com.guilherme.alura.forumalura.model.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
     public List<Topico> lista() {
-        Topico topico = new Topico("Duvida Spring boot",
+        Topico topico = new Topico("Dúvida Spring boot",
                 "Tenho uma duvida sobre spring boot",
                 new Curso("SpringBoot 1", "Programação"));
 
