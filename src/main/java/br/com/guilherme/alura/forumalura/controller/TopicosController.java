a2ef420e079b4cbc868de0b055bf15633b3a96b3
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-21 15:06:54 -0300

    Criando recurso para detalhar um topico do forum

--- src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
+++ src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.guilherme.alura.forumalura.controller.form.TopicoForm;
+import br.com.guilherme.alura.forumalura.dto.DetalhesDoTopicoDTO;
 import br.com.guilherme.alura.forumalura.dto.TopicoDTO;
 import br.com.guilherme.alura.forumalura.model.Topico;
 import br.com.guilherme.alura.forumalura.repository.CursoRepository;
@@ -31,7 +33,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
     
 	@GetMapping
-    public List<TopicoDTO> lista(String nomeCurso) {
+    public List<TopicoDTO> listar(String nomeCurso) {
     	if(nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
             return TopicoDTO.converter(topicos);
@@ -48,5 +50,11 @@ public class TopicosController {
     	
     	URI uri = builderURI.path("/topicos/{Id}").buildAndExpand(topico.getId()).toUri();
     	return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+    }
+    
+    @GetMapping("/{id}")
+    public DetalhesDoTopicoDTO detalhar(@PathVariable("id") Long id) {
+    	Topico topico = topicoRepository.getOne(id);
+    	return new DetalhesDoTopicoDTO(topico);
     }
 }
