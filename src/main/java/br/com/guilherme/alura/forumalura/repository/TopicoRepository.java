2509a018384c7f9301fe652fbe77919cdda6c7a3
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-21 01:23:13 -0300

    Criando recurso para retornar topico pelo nome

--- src/main/java/br/com/guilherme/alura/forumalura/repository/TopicoRepository.java
+++ src/main/java/br/com/guilherme/alura/forumalura/repository/TopicoRepository.java
@@ -1,5 +1,7 @@
 package br.com.guilherme.alura.forumalura.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import br.com.guilherme.alura.forumalura.model.Topico;
 
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-
+	
+	List<Topico> findByCursoNome(String nomeCurso);
 }
