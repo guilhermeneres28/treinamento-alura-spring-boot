8e424720a895ae611a1d0842db0b95f33d73872f
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-22 16:44:49 -0300

    adicionando ordenacao no recurso que lista topicos

--- src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
+++ src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,9 +45,10 @@ public class TopicosController {
 	@GetMapping
     public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, 
     								@RequestParam int pagina, 
-    								@RequestParam int quantidade) {
+    								@RequestParam int quantidade,
+    								@RequestParam String ordenacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
+		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
 		
     	if(nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
