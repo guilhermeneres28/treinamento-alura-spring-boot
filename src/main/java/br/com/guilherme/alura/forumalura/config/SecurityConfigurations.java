a09f013c0919fc76fc0b3dfe20de6f81ac973da3
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-24 18:40:01 -0300

    habilitando spring security

---
+++ src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package br.com.guilherme.alura.forumalura.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+}
