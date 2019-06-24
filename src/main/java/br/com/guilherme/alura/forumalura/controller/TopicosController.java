475b3770c9f8fd88e523049d56e3fd06af263918
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-24 18:17:11 -0300

    invalidando cache de lista de topicos no momento de salvar um topico

--- src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
+++ src/main/java/br/com/guilherme/alura/forumalura/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -57,6 +58,7 @@ public class TopicosController {
     
     @PostMapping
     @Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoFormDTO form, UriComponentsBuilder builderURI) {
 		Topico topico = form.converter(cursoRepository);
     	topicoRepository.save(topico);
