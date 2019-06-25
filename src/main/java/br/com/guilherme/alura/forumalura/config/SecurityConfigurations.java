6d20829d9034f03aeaf173dfba79b51f097c0762
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-25 17:10:15 -0300

    * Implementação do autentication manager

--- src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
+++ src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package br.com.guilherme.alura.forumalura.config;
 
+import br.com.guilherme.alura.forumalura.service.AutenticacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,22 +9,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     // Configuração de autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     // Configuração de autorização
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();
@@ -31,6 +36,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Configuraçõe de recursos estaticos: js, css, imagens
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+
     }
 }
