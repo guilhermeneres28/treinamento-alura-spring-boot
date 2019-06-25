cac3cbbed87e451515b12ca44fea00f0be014994
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-25 17:38:00 -0300

    adicionando lib para criar jwt com java e retirando autenticacao com sessao da aplicacao

--- src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
+++ src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -28,9 +29,11 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos/").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     // Configuraçõe de recursos estaticos: js, css, imagens
