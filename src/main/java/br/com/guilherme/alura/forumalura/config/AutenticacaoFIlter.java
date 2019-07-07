dd3537bb62c4e13cee56d514215a7b35e091fda6
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-07-07 17:59:47 -0300

    Implementando filtro para realizar logica de verificacao do Bearer token

---
+++ src/main/java/br/com/guilherme/alura/forumalura/config/AutenticacaoFIlter.java
@@ -0,0 +1,35 @@
+package br.com.guilherme.alura.forumalura.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AutenticacaoFIlter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = recuperarToken(request);
+		System.out.println("Token is here".concat(token));
+		
+		filterChain.doFilter(request, response);
+		
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		
+		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		
+		 
+		return token.substring(7, token.length());
+	}
+}
--- src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
+++ src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -40,7 +41,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoFIlter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     // Configuraçõe de recursos estaticos: js, css, imagens
