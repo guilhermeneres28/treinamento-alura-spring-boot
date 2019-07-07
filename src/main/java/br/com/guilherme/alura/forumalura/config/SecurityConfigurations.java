a0c4b4a742ff29837cb4e74d22517f064527da8a
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-07-07 18:13:57 -0300

    Permitindo rota para autenticacao

--- src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
+++ src/main/java/br/com/guilherme/alura/forumalura/config/SecurityConfigurations.java
@@ -38,7 +38,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos/").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
